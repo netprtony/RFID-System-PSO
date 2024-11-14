447ba1d99ec7b167086d18f9e6a45a76c1b399ad
netprtony <huynhvikhang6a13@gmail.com>
2024-11-14 13:01:03 +0700

    w

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ DIM = 2
 NUM_RFID_READERS = 3
 if __name__ == "__main__":
     tags = [Tags(DIM) for _ in range(NUM_INDIVIDUALS)]
-    #readers = selection_mechanism(tags, NUM_RFID_READERS)
-    #sspso = SSPSO(NUM_RFID_READERS, DIM, NUM_ITERATION, readers)
-    #mainOptimization(tags, readers, sspso)
+    readers = selection_mechanism(tags, NUM_RFID_READERS)
+    sspso = SSPSO(NUM_RFID_READERS, DIM, NUM_ITERATION, readers)
+    mainOptimization(tags, readers, sspso)
     #BieuDoReader(readers, tags)
-    clusters = birch_clustering(tags)
-    print(len(clusters))
+    #clusters = birch_clustering(tags)
+    #print(len(clusters))
     # Khởi tạo các reader tại các vị trí của các clusters
-    readers = [Readers(position=cluster) for cluster in clusters]
-    BieuDoReader(readers, tags)
+    #readers = [Readers(position=cluster) for cluster in clusters]
+    #BieuDoReader(readers, tags)
